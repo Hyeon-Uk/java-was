462ffbcd196b10ff06a5455e7bb032b146c2a4fc
Hyeon-Uk <rlagusdnr120@gmail.com>
2024-07-03 12:29:05 +0900

    [Test] queryString 메서드에 대한 테스트코드 추가
    - 있으면 그걸 가져온다.
    - 공백이면 공백값을 가져온다.
    - 없는 파라미터를 호출하면 Null을 가져온다.
    - URL Encoding된 값은 Decoding하여 가져온다.

--- src/test/java/codesquad/message/request/HttpRequestMessageTest.java
+++ src/test/java/codesquad/message/request/HttpRequestMessageTest.java
@@ -54,4 +54,98 @@ class HttpRequestMessageTest {
         });
     }
 
+    @Test
+    public void queryStringMessage(){
+        //given
+        String queryStringMessage = """
+            GET /create?username=hyeonuk&nickname=khu147&password=password1234 HTTP/1.1
+            Host: localhost:8080
+            User-Agent: Mozilla/5.0
+            Accept: text/html,application/xhtml+xml,application/xml;q=0.9,application/json
+            Accept-Language: ko-KR,ko;q=0.9,zh-CN,zh;q=0.8
+            Accept-Encoding: gzip, deflate, sdch
+            Connection: keep-alive
+            Upgrade-Insecure-Requests: 1
+            Content-Type: application/x-www-form-urlencoded
+            
+            """;
+        //when
+        HttpRequestMessage req = new HttpRequestMessage(queryStringMessage);
+
+        //then
+        assertAll("queryStringValidation",
+                ()->assertEquals("hyeonuk",req.getQueryString("username")),
+                ()->assertEquals("khu147",req.getQueryString("nickname")),
+                ()->assertEquals("password1234",req.getQueryString("password"))
+                );
+    }
+    @Test
+    public void emptyQueryStringMessage(){
+        //given
+        String queryStringMessage = """
+            GET /create?username=&nickname= HTTP/1.1
+            Host: localhost:8080
+            User-Agent: Mozilla/5.0
+            Accept: text/html,application/xhtml+xml,application/xml;q=0.9,application/json
+            Accept-Language: ko-KR,ko;q=0.9,zh-CN,zh;q=0.8
+            Accept-Encoding: gzip, deflate, sdch
+            Connection: keep-alive
+            Upgrade-Insecure-Requests: 1
+            Content-Type: application/x-www-form-urlencoded
+            
+            """;
+        //when
+        HttpRequestMessage req = new HttpRequestMessage(queryStringMessage);
+
+        //then
+        assertAll("blank return blank string",
+                ()->assertEquals("",req.getQueryString("username")),
+                ()->assertEquals("",req.getQueryString("nickname")));
+    }
+
+    @Test
+    public void nullQueryStringMessage(){
+        //given
+        String queryStringMessage = """
+            GET /create HTTP/1.1
+            Host: localhost:8080
+            User-Agent: Mozilla/5.0
+            Accept: text/html,application/xhtml+xml,application/xml;q=0.9,application/json
+            Accept-Language: ko-KR,ko;q=0.9,zh-CN,zh;q=0.8
+            Accept-Encoding: gzip, deflate, sdch
+            Connection: keep-alive
+            Upgrade-Insecure-Requests: 1
+            Content-Type: application/x-www-form-urlencoded
+            
+            """;
+        //when
+        HttpRequestMessage req = new HttpRequestMessage(queryStringMessage);
+
+        //then
+        assertAll("not exists query parameter return null",
+                ()->assertNull(req.getQueryString("notExists")));
+    }
+
+    @Test
+    public void urlEncodingQueryString(){
+        //given
+        //안녕하세요 url 인코딩 = %ec%95%88%eb%85%95%ed%95%98%ec%84%b8%ec%9a%94
+        String queryStringMessage = """
+            GET /create?name=%ec%95%88%eb%85%95%ed%95%98%ec%84%b8%ec%9a%94 HTTP/1.1
+            Host: localhost:8080
+            User-Agent: Mozilla/5.0
+            Accept: text/html,application/xhtml+xml,application/xml;q=0.9,application/json
+            Accept-Language: ko-KR,ko;q=0.9,zh-CN,zh;q=0.8
+            Accept-Encoding: gzip, deflate, sdch
+            Connection: keep-alive
+            Upgrade-Insecure-Requests: 1
+            Content-Type: application/x-www-form-urlencoded
+            
+            """;
+        //when
+        HttpRequestMessage req = new HttpRequestMessage(queryStringMessage);
+
+        //then
+        assertEquals("안녕하세요",req.getQueryString("name"));
+    }
 }
