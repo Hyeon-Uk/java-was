dd3f2b7c2b07ab141cb7202c0b81ef4b5ce0daad
Hyeon-Uk <rlagusdnr120@gmail.com>
2024-07-15 22:38:45 +0900

    [Feat] RequestHandler 인터페이스를 handle로 처리할 수 있도록 수정

--- src/main/java/codesquad/was/http/handler/RequestHandler.java
+++ src/main/java/codesquad/was/http/handler/RequestHandler.java
@@ -8,45 +8,5 @@ import codesquad.was.utils.FileUtils;
 
 
 public interface RequestHandler {
-    default void handle(HttpRequest req, HttpResponse res){
-        switch(req.getMethod()){
-            case GET->getHandle(req,res);
-            case POST ->postHandle(req,res);
-            case PUT ->putHandle(req,res);
-            case PATCH -> patchHandle(req,res);
-            case DELETE -> deleteHandle(req,res);
-            case OPTIONS -> optionsHandle(req,res);
-            case TRACE -> traceHandle(req,res);
-            case HEAD -> headHandle(req,res);
-            case CONNECT -> connectHandle(req,res);
-        }
-    }
-
-    default void getHandle(HttpRequest req, HttpResponse res){
-        throw new HttpMethodNotAllowedException("This method is not allowed");
-    }
-    default void postHandle(HttpRequest req, HttpResponse res){
-        throw new HttpMethodNotAllowedException("This method is not allowed");
-    }
-    default void putHandle(HttpRequest req, HttpResponse res){
-        throw new HttpMethodNotAllowedException("This method is not allowed");
-    }
-    default void patchHandle(HttpRequest req, HttpResponse res){
-        throw new HttpMethodNotAllowedException("This method is not allowed");
-    }
-    default void deleteHandle(HttpRequest req, HttpResponse res){
-        throw new HttpMethodNotAllowedException("This method is not allowed");
-    }
-    default void optionsHandle(HttpRequest req, HttpResponse res){
-        throw new HttpMethodNotAllowedException("This method is not allowed");
-    }
-    default void traceHandle(HttpRequest req, HttpResponse res){
-        throw new HttpMethodNotAllowedException("This method is not allowed");
-    }
-    default void headHandle(HttpRequest req, HttpResponse res){
-        throw new HttpMethodNotAllowedException("This method is not allowed");
-    }
-    default void connectHandle(HttpRequest req, HttpResponse res){
-        throw new HttpMethodNotAllowedException("This method is not allowed");
-    }
+    void handle(HttpRequest req, HttpResponse res);
 }
