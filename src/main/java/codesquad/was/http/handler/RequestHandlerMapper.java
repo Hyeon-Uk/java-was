30c845a1422f6ca5a7cc4879f7aec8ea3149887a
Hyeon-Uk <rlagusdnr120@gmail.com>
2024-07-15 22:38:30 +0900

    [Feat] RequestHandlerMapper에서 path와 method, handler를 입력받아 등록할 수 있도록 인터페이스 수정

--- src/main/java/codesquad/was/http/handler/RequestHandlerMapper.java
+++ src/main/java/codesquad/was/http/handler/RequestHandlerMapper.java
@@ -1,6 +1,8 @@
 package codesquad.was.http.handler;
 
+import codesquad.was.http.message.request.HttpMethod;
+
 public interface RequestHandlerMapper {
-    RequestHandler getRequestHandler(String path);
-    void setRequestHandler(String path,RequestHandler requestHandler);
+    RequestHandler getRequestHandler(String path,HttpMethod method);
+    void setRequestHandler(String path, HttpMethod method,RequestHandler requestHandler);
 }
--- src/main/java/codesquad/was/http/handler/RequestHandlerMapperImpl.java
+++ src/main/java/codesquad/was/http/handler/RequestHandlerMapperImpl.java
@@ -1,10 +1,10 @@
 package codesquad.was.http.handler;
 
-import codesquad.application.handler.*;
 import codesquad.framework.coffee.annotation.Coffee;
 import codesquad.framework.coffee.annotation.Named;
 import codesquad.middleware.UserDatabase;
 import codesquad.was.http.exception.HttpNotFoundException;
+import codesquad.was.http.message.request.HttpMethod;
 import codesquad.was.utils.FileUtil;
 
 import java.util.HashMap;
@@ -13,47 +13,34 @@ import java.util.Map;
 @Coffee
 public class RequestHandlerMapperImpl implements RequestHandlerMapper{
     private final static String STATIC_RESOURCE_KEY = "staticResourceKey";
-    private final RequestHandler staticResourceHandler;
-    private final DefaultHandler defaultHandler;
-    private final Map<String, RequestHandler> mappers;
-    private final UserDatabase userDatabase;
+    private final Map<String, Map<HttpMethod,RequestHandler>> mappers;
     private final FileUtil fileUtil;
-    public RequestHandlerMapperImpl(FileUtil fileUtil,
-                                    UserDatabase userDatabase) {
-        this.userDatabase = userDatabase;
+    private final RequestHandler staticResourceHandler;
+    public RequestHandlerMapperImpl(FileUtil fileUtil) {
         this.fileUtil = fileUtil;
         this.staticResourceHandler = new StaticResourceHandler(fileUtil);
-        this.defaultHandler = new DefaultHandler(fileUtil);
-//        mappers = Map.of(
-//                STATIC_RESOURCE_KEY, staticResourceHandler,
-//                "/user/create",new RegisterHandler(userDatabase),
-//                "/login",new LoginHandler(userDatabase),
-//                "/logout",new LogoutHandler(),
-//                "/user/list",new UserListHandler(userDatabase),
-//                "/",new MainHandler(fileUtil),
-//                "/registration",new RegisterHandler(userDatabase)
-//        );
         mappers = new HashMap<>();
-        setRequestHandler(STATIC_RESOURCE_KEY,staticResourceHandler);
     }
 
     @Override
-    public void setRequestHandler(String path,RequestHandler requestHandler) {
-        mappers.put(path,requestHandler);
+    public void setRequestHandler(String path,HttpMethod method,RequestHandler requestHandler) {
+        mappers.computeIfAbsent(path, k->new HashMap<>())
+                .put(method,requestHandler);
     }
 
-    public RequestHandler getRequestHandler(String path) {
+    public RequestHandler getRequestHandler(String path,HttpMethod method) {
         if (isStaticFileRequest(path)) {
-            return mappers.get(STATIC_RESOURCE_KEY);
+            return staticResourceHandler;
         }
-        return mappers.entrySet()
-                .stream()
-                .sorted((o1,o2)->Integer.compare(o2.getKey().length(),o1.getKey().length()))
-                .filter(entry -> path.equals(entry.getKey()))
-                .findFirst()
-                .map(Map.Entry::getValue)
-//                .orElseThrow(() -> new HttpNotFoundException(path.concat(" : request can not found")));
-                .orElse(defaultHandler);
+        Map<HttpMethod,RequestHandler> requestHandlers = mappers.get(path);
+        if(requestHandlers == null){
+            throw new HttpNotFoundException(path+" not found");
+        }
+        RequestHandler requestHandler = requestHandlers.get(method);
+        if(requestHandler == null){
+            throw new HttpNotFoundException(path+" not found");
+        }
+        return requestHandler;
     }
 
     private boolean isStaticFileRequest(String path) {
