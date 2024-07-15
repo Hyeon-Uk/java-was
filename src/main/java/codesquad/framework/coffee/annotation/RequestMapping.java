af85db6dabe93bb65eaaa0d9d7efc36469f21b7d
Hyeon-Uk <rlagusdnr120@gmail.com>
2024-07-15 22:39:55 +0900

    [Feat] handler를 메서드 레벨에서 처리할 수 있도록 Controller와 RequestMapping 어노테이션 추가

--- src/main/java/codesquad/framework/coffee/annotation/RequestMapping.java
+++ src/main/java/codesquad/framework/coffee/annotation/RequestMapping.java
@@ -1,6 +1,7 @@
 package codesquad.framework.coffee.annotation;
 
 import codesquad.was.http.message.request.HttpMethod;
+import codesquad.was.http.message.response.HttpStatus;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -8,7 +9,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.TYPE)
+@Target(ElementType.METHOD)
 public @interface RequestMapping {
     String path() default "";
     HttpMethod method() default HttpMethod.GET;
