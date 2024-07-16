a9a9a1e50d6416a1ac1804e37d73f9b91cc158ae
Hyeon-Uk <rlagusdnr120@gmail.com>
2024-07-16 16:51:18 +0900

    [Feat&Test] 게시글 작성 페이지 조회 컨트롤러 구현 및 테스트
    1. 로그인한 상태로 접근 가능하다
    2. 로그인하지 않은 상태 혹은 세션이 없는 상태로는 로그인페이지로 리다이렉트된다.

---
+++ src/main/java/codesquad/application/handler/BoardController.java
@@ -0,0 +1,30 @@
+package codesquad.application.handler;
+
+import codesquad.application.model.User;
+import codesquad.framework.coffee.annotation.Coffee;
+import codesquad.framework.coffee.annotation.Controller;
+import codesquad.framework.coffee.annotation.RequestMapping;
+import codesquad.framework.dispatcher.mv.Model;
+import codesquad.was.http.message.request.HttpMethod;
+import codesquad.was.http.session.Session;
+
+import java.util.Optional;
+
+@Controller
+@Coffee
+public class BoardController {
+    @RequestMapping(path = "/write",method= HttpMethod.GET)
+    public String writePage(Session session, Model model){
+        if(session == null){
+            return "redirect:/login";
+        }
+
+        Optional<Object> user1 = session.get("user");
+        if(user1.isEmpty()) return "redirect:/login";
+
+        User user = (User)user1.get();
+        model.addAttribute("user",user);
+        model.addAttribute("name",user.getNickname());
+        return "article/index";
+    }
+}
---
+++ src/test/java/codesquad/application/handler/BoardControllerTest.java
@@ -0,0 +1,60 @@
+package codesquad.application.handler;
+
+import codesquad.application.model.User;
+import codesquad.framework.dispatcher.mv.Model;
+import codesquad.was.http.session.Session;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BoardControllerTest {
+    BoardController boardController = new BoardController();
+
+    @Nested
+    @DisplayName("write page")
+    class WritePage {
+        Session session = new Session(new Date(),new Date());
+        Model model = new Model();
+        User user = new User("id1","password1","nickname1");
+
+        @Test
+        void success(){
+            //given
+            session.set("user",user);
+
+            //when
+            String path = boardController.writePage(session, model);
+
+            //then
+            assertEquals("article/index",path);
+            assertEquals(user,model.getAttribute("user"));
+            assertEquals(user.getNickname(),model.getAttribute("name"));
+        }
+
+        @Test
+        void sessionIsNull(){
+            //given
+
+            //when
+            String path = boardController.writePage(null, model);
+
+            //then
+            assertEquals("redirect:/login",path);
+        }
+
+        @Test
+        void sessionUserIsNull(){
+            //given
+
+            //when
+            String path = boardController.writePage(session, model);
+
+            //then
+            assertEquals("redirect:/login",path);
+        }
+    }
+}
