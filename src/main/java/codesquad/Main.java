31fe1f7dfad188d10d9c99b0c47f0e1153792594
Hyeon-Uk <rlagusdnr120@gmail.com>
2024-07-16 09:44:06 +0900

    [Feat] RequestMapping 메서드의 argument를 동적으로 분석한 뒤 주입하고, String을 반환하면 해당 path에 맞는 view를 render하도록 구현

--- src/main/java/codesquad/Main.java
+++ src/main/java/codesquad/Main.java
@@ -3,9 +3,16 @@ package codesquad;
 import codesquad.framework.coffee.CoffeeShop;
 import codesquad.framework.coffee.annotation.Controller;
 import codesquad.framework.coffee.annotation.RequestMapping;
+import codesquad.framework.coffee.mvc.Model;
 import codesquad.was.Server;
+import codesquad.was.http.engine.HttpTemplateEngine;
 import codesquad.was.http.handler.RequestHandler;
 import codesquad.was.http.handler.RequestHandlerMapper;
+import codesquad.was.http.message.request.HttpRequest;
+import codesquad.was.http.message.response.HttpResponse;
+import codesquad.was.http.message.response.HttpStatus;
+import codesquad.was.http.session.Session;
+import codesquad.was.utils.FileUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,10 +24,48 @@ import java.util.List;
 public class Main {
     private final static Logger logger = LoggerFactory.getLogger(Main.class);
 
-    private RequestHandler makeRequestHandler(Object bean, Method method) {
+    private Object[] resolveArguments(HttpRequest req, HttpResponse res,Model model,Method method){
+        return Arrays.stream(method.getParameters())
+                .map(param ->{
+                    Class<?> type = param.getType();
+                    if(HttpRequest.class.equals(type)) return req;
+                    else if(HttpResponse.class.equals(type)) return res;
+                    else if(Session.class.equals(type)) return req.getSession(true);
+                    else if(Model.class.equals(type)) return model;
+                    else return null;
+                }).toArray();
+    }
+
+
+    private RequestHandler makeRequestHandler(Object bean, Method method, HttpStatus status, FileUtil fileUtil, HttpTemplateEngine engine) {
         return (req, res) -> {
             try {
-                method.invoke(bean, req, res);
+                Model model = new Model();
+                Object[] args = resolveArguments(req, res,model, method);
+                Class<?> returnType = method.getReturnType();
+                if(returnType.equals(String.class)){//view처리
+                    String viewPath = (String)method.invoke(bean, args);
+
+                    if(viewPath.startsWith("redirect:")){
+                        String redirectPath = viewPath.substring("redirect:".length());
+
+                        res.sendRedirect(redirectPath);
+                        return;
+                    }
+
+                    if(!viewPath.startsWith("/")){
+                        viewPath = "/" + viewPath;
+                    }
+                    String fullViewPath = viewPath.concat(".html");
+                    String html = new String(fileUtil.readStaticFile(fullViewPath));
+                    String rendered = engine.render(html, model.asMap());
+
+                    res.setStatus(status);
+                    res.setBody(rendered.getBytes());
+                }
+                else{
+                    method.invoke(bean,args);
+                }
             } catch (Exception e) {
                 throw new IllegalStateException("Failed to invoke method " + method + " on " + bean, e);
             }
@@ -30,13 +75,15 @@ public class Main {
     public Main(int port) throws Exception {
         CoffeeShop coffeeShop = new CoffeeShop();
         RequestHandlerMapper requestHandlerMapper = coffeeShop.getBean(RequestHandlerMapper.class);
+        HttpTemplateEngine engine = coffeeShop.getBean(HttpTemplateEngine.class);
+        FileUtil fileUtil = coffeeShop.getBean(FileUtil.class);
         List<?> controllerClasses = coffeeShop.getAllBeansOfAnnotation(Controller.class);
         for (Object controllerClass : controllerClasses) {
             Arrays.stream(controllerClass.getClass().getDeclaredMethods())
                     .filter(method -> method.isAnnotationPresent(RequestMapping.class))
                     .forEach(method -> {
                         RequestMapping annotation = method.getAnnotation(RequestMapping.class);
-                        requestHandlerMapper.setRequestHandler(annotation.path(), annotation.method(), makeRequestHandler(controllerClass, method));
+                        requestHandlerMapper.setRequestHandler(annotation.path(), annotation.method(), makeRequestHandler(controllerClass, method,annotation.status(),fileUtil,engine));
                     });
         }
 
