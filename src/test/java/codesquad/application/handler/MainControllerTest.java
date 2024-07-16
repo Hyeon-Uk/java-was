c3fabcee1e230ed15685150bae177387a3bda0cf
Hyeon-Uk <rlagusdnr120@gmail.com>
2024-07-16 12:36:03 +0900

    [Test] main controller의 테스트코드 추가
    1. login한 상태(session에 값이 있는 상태) 로 접속하면 model에 알맞은 값이 셋팅됨
    2. login하지 않아서 session이 null인 상태라면 model에 값이 셋팅되지않음
    3. session이 있지만 내부 user가 null인 상태라면 model에 값이 셋팅되지 않음

---
+++ src/test/java/codesquad/application/handler/MainControllerTest.java
@@ -0,0 +1,76 @@
+package codesquad.application.handler;
+
+import codesquad.application.handler.mock.MockUserDatabase;
+import codesquad.application.model.User;
+import codesquad.framework.coffee.mvc.Model;
+import codesquad.message.mock.MockTimer;
+import codesquad.middleware.UserDatabase;
+import codesquad.was.http.session.Session;
+import codesquad.was.utils.Timer;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+class MainControllerTest {
+    private UserDatabase userDatabase = new MockUserDatabase();
+    private MainController controller = new MainController(userDatabase);
+    private long currentTime = 1000l;
+    private Timer timer = new MockTimer(currentTime);
+    @Test
+    void mainPageWithLogin(){
+        //given
+        Session session = new Session(timer.getCurrentTime(),timer.getCurrentTime());
+        User user = new User("id", "password", "nickname");
+        session.set("user",user);
+        Model model = new Model();
+
+        //when
+        String path = controller.mainPage(session, model);
+
+        //then
+        assertEquals("index",path);
+        assertEquals(user,model.getAttribute("user"));
+        assertEquals(user.getNickname(),model.getAttribute("name"));
+    }
+
+    @Test
+    void mainPageWithoutLogin1(){
+        //given
+        Model model = new Model();
+
+        //when
+        String path = controller.mainPage(null, model);
+
+        //then
+        assertEquals("index",path);
+        assertNull(model.getAttribute("user"));
+        assertNull(model.getAttribute("name"));
+    }
+
+    @Test
+    void mainPageWithEmptySession(){
+        //given
+        Model model = new Model();
+        Session session = new Session(timer.getCurrentTime(),timer.getCurrentTime());
+
+        //when
+        String path = controller.mainPage(session, model);
+
+        //then
+        assertEquals("index",path);
+        assertNull(model.getAttribute("user"));
+        assertNull(model.getAttribute("name"));
+    }
+
+    @Test
+    void loginFailedPage(){
+        //given
+
+        //when
+        String path = controller.loginFailed();
+
+        //then
+        assertEquals("user/login_failed",path);
+    }
+}
