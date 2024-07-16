7cc00df956c2d334be28561221a787dfac89445b
Hyeon-Uk <rlagusdnr120@gmail.com>
2024-07-16 13:47:15 +0900

    [Test] UserAuthController의 login메서드를 검증하는 테스트코드 작성
    1. 유저의 로그인이 성공하는 케이스
    2. 존재하지않는 아이디로 로그인했을경우
    3. 아이디는 존재하지만 password가 일치하지 않는 경우

--- src/test/java/codesquad/application/handler/UserAuthControllerTest.java
+++ src/test/java/codesquad/application/handler/UserAuthControllerTest.java
@@ -8,6 +8,8 @@ import codesquad.middleware.UserDatabase;
 import codesquad.was.http.exception.HttpBadRequestException;
 import codesquad.was.http.message.request.HttpMethod;
 import codesquad.was.http.message.request.HttpRequest;
+import codesquad.was.http.message.response.HttpResponse;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -26,7 +28,10 @@ class UserAuthControllerTest {
     @Nested
     @DisplayName("Register")
     class RegisterTest{
-
+        /**
+         * 1. 정상적인 회원가입 로직
+         * 2. 이미 존재하는 id로 가입하려했을때 에러
+         */
         @Test
         void registerUserSuccess(){
             //given
@@ -70,6 +75,69 @@ class UserAuthControllerTest {
             assertThrows(HttpBadRequestException.class,()->{
                 userAuthController.registerUser(request);
             });
+        }
+    }
+
+    @Nested
+    @DisplayName("login")
+    class LoginTest{
+        /**
+         * 1. 유저의 로그인이 성공하는 케이스
+         * 2. 존재하지않는 아이디로 로그인했을경우
+         * 3. 아이디는 존재하지만 password가 일치하지 않는 경우
+         */
+        private String id = "id";
+        private String password = "password";
+        private String nickname = "nickname";
+        @BeforeEach
+        void userSetUp(){
+            userDatabase.save(new User(id,passwordEncoder.encode(password),nickname));
+        }
+
+        //TODO : 로그인이 끝난 뒤 session에 객체가 남아있는것을 검증해야함
+        @Test
+        void loginSuccess(){
+            //given
+            Map<String,String> queryString = new HashMap<>();
+            queryString.put("userId",id);
+            queryString.put("password",password);
+            HttpRequest req = MockFactory.getHttpRequest(HttpMethod.POST,queryString,new HashMap<>(),"");
+            HttpResponse res = MockFactory.getHttpResponse();
+            //when
+            String path = userAuthController.loginProcess(req, res);
+
+            //then
+            assertEquals("redirect:/",path);
+        }
+
+        @Test
+        void loginFailedWithNonExistsUser(){
+            //given
+            Map<String,String> queryString = new HashMap<>();
+            queryString.put("userId","nonExistsUser");
+            queryString.put("password",password);
+            HttpRequest req = MockFactory.getHttpRequest(HttpMethod.POST,queryString,new HashMap<>(),"");
+            HttpResponse res = MockFactory.getHttpResponse();
+            //when
+            String path = userAuthController.loginProcess(req, res);
+
+            //then
+            assertEquals("redirect:/user/login_failed",path);
+        }
+
+        @Test
+        void loginFailedWithWrongPassword(){
+            //given
+            Map<String,String> queryString = new HashMap<>();
+            queryString.put("userId",id);
+            queryString.put("password","wrongPassword");
+            HttpRequest req = MockFactory.getHttpRequest(HttpMethod.POST,queryString,new HashMap<>(),"");
+            HttpResponse res = MockFactory.getHttpResponse();
+            //when
+            String path = userAuthController.loginProcess(req, res);
+
+            //then
+            assertEquals("redirect:/user/login_failed",path);
         }
     }
 }
