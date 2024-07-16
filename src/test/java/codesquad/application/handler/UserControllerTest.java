cff95075c2f7119a30b90e9b7b2b6fb29a446133
Hyeon-Uk <rlagusdnr120@gmail.com>
2024-07-16 13:53:50 +0900

    [Test] registration 페이지 요청에 대한 테스트코드 추가

---
+++ src/test/java/codesquad/application/handler/UserControllerTest.java
@@ -0,0 +1,29 @@
+package codesquad.application.handler;
+
+import codesquad.application.handler.mock.MockUserDatabase;
+import codesquad.middleware.UserDatabase;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserControllerTest {
+    private UserDatabase userDatabase = new MockUserDatabase();
+    private UserController userController = new UserController(userDatabase);
+
+    @Nested
+    @DisplayName("registration")
+    class Registration {
+        @Test
+        void registrationPageTest(){
+            //given
+
+            //when
+            String path = userController.registration();
+
+            //then
+            assertEquals("registration/index",path);
+        }
+    }
+}
