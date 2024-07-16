4d1d0dedaa260ece960d7ab7f75975819741cd8a
Hyeon-Uk <rlagusdnr120@gmail.com>
2024-07-16 11:11:01 +0900

    [Feat] UserDatabase의 기능을 인터페이스로 분리

--- src/main/java/codesquad/middleware/UserDatabase.java
+++ src/main/java/codesquad/middleware/UserDatabase.java
@@ -1,33 +1,14 @@
 package codesquad.middleware;
 
 import codesquad.application.model.User;
-import codesquad.framework.coffee.annotation.Coffee;
 
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
-import java.util.concurrent.ConcurrentHashMap;
 
-@Coffee
-public class UserDatabase {
-    public UserDatabase() {}
-    private final static Map<String,User> store = new ConcurrentHashMap<>();
+public interface UserDatabase {
+    void save(User user);
 
-    public void save(User user) {
-        store.put(user.getId(),user);
-    }
+    Optional<User> findById(String userId);
 
-    public Optional<User> findById(String userId) {
-        return store.entrySet()
-                .stream()
-                .filter(entry -> entry.getKey().equals(userId))
-                .findFirst()
-                .map(Map.Entry::getValue);
-    }
-
-    public List<User> findAll(){
-        return store.values()
-                .stream()
-                .toList();
-    }
+    List<User> findAll();
 }
