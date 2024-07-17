7872858567f8d67250e18bd93b88393e9820d007
Hyeon-Uk <rlagusdnr120@gmail.com>
2024-07-17 10:48:33 +0900

    [Feat] Board regist 비즈니스 로직 작성 및 테스트코드 작성
    csrf토큰을 비교하여 일치하면 save하도록 비즈니스 로직을 작성

--- src/main/java/codesquad/application/handler/BoardController.java
+++ src/main/java/codesquad/application/handler/BoardController.java
@@ -1,13 +1,16 @@
 package codesquad.application.handler;
 
+import codesquad.application.dto.BoardRegist;
+import codesquad.application.model.Board;
 import codesquad.application.model.User;
 import codesquad.framework.coffee.annotation.Coffee;
 import codesquad.framework.coffee.annotation.Controller;
 import codesquad.framework.coffee.annotation.RequestMapping;
 import codesquad.framework.dispatcher.mv.Model;
+import codesquad.framework.resolver.annotation.RequestParam;
 import codesquad.framework.resolver.annotation.SessionParam;
+import codesquad.middleware.BoardDatabase;
 import codesquad.was.http.message.request.HttpMethod;
-import codesquad.was.http.message.request.HttpRequest;
 import codesquad.was.http.session.Session;
 
 import java.util.Optional;
@@ -16,6 +19,11 @@ import java.util.UUID;
 @Controller
 @Coffee
 public class BoardController {
+    private final BoardDatabase boardDatabase;
+    public BoardController(BoardDatabase boardDatabase) {
+        this.boardDatabase = boardDatabase;
+    }
+
     @RequestMapping(path = "/write",method= HttpMethod.GET)
     public String writePage(@SessionParam(create=false) Session session, Model model){
         if(session == null){
@@ -33,5 +41,33 @@ public class BoardController {
         model.addAttribute("csrfToken",csrfToken);
         session.set("csrfToken",csrfToken);
         return "article/index";
+    }
+
+    @RequestMapping(path ="/write",method=HttpMethod.POST)
+    public String registBoard(@SessionParam(create = false) Session session,
+                              @RequestParam BoardRegist req){
+        if(session == null){
+            return "redirect:/login";
+        }
+        String sessionCsrf = session.get("csrfToken")
+                                .map(String.class::cast)
+                                .orElse(null);
+
+        if(sessionCsrf == null) return "redirect:/";
+
+        Optional<User> sessionUser = session.get("user").map(User.class::cast);
+        if(sessionUser.isEmpty()) return "redirect:/login";
+        User user = sessionUser.get();
+        String title = req.getTitle();
+        String content = req.getContent();
+        String csrfToken = req.getCsrfToken();
+        String writer = user.getId();
+
+        if(csrfToken != null && csrfToken.equals(sessionCsrf)){
+            Board board = new Board(title,content,writer);
+            boardDatabase.save(board);
+            return "redirect:/";
+        }
+        return "redirect:/";
     }
 }
--- src/test/java/codesquad/application/handler/BoardControllerTest.java
+++ src/test/java/codesquad/application/handler/BoardControllerTest.java
@@ -1,18 +1,24 @@
 package codesquad.application.handler;
 
+import codesquad.application.dto.BoardRegist;
+import codesquad.application.handler.mock.MockBoardDatabase;
+import codesquad.application.model.Board;
 import codesquad.application.model.User;
 import codesquad.framework.dispatcher.mv.Model;
+import codesquad.middleware.BoardDatabase;
 import codesquad.was.http.session.Session;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.Date;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BoardControllerTest {
-    BoardController boardController = new BoardController();
+    private BoardDatabase boardDatabase = new MockBoardDatabase();
+    private BoardController boardController = new BoardController(boardDatabase);
 
     @Nested
     @DisplayName("write page")
@@ -56,6 +62,146 @@ class BoardControllerTest {
 
             //then
             assertEquals("redirect:/login",path);
+        }
+    }
+
+    @Nested
+    @DisplayName("regist board")
+    class RegistBoard {
+        /**
+         * 1. 정상적으로 수행후 redirect /
+         * 2. csrf토큰이 일치하지 않으면 redirect /
+         * 3. csrf토큰이 존재하지 않으면 redirect /
+         * 4. session이 존재하지 않으면 redirect login
+         * 5. session에 user가 존재하지 않으면 redirect login
+         * 6. board의 csrf토큰이 null일 경우 redirect /
+         */
+
+        @Test
+        void success(){
+            //given
+            Session session = new Session(new Date(),new Date());
+            User user = new User("id","password","nickname");
+            String csrfToken = UUID.randomUUID().toString();
+            session.set("user",user);
+            session.set("csrfToken",csrfToken);
+
+            BoardRegist board = new BoardRegist("title","content",csrfToken);
+
+            //when
+            String path = boardController.registBoard(session, board);
+
+            //then
+            assertEquals("redirect:/",path);
+            long size = boardDatabase.findAll()
+                    .stream().filter(b -> b.getWriter().equals("id"))
+                    .count();
+            assertEquals(1,size);
+        }
+
+        @Test
+        void notMathCsrfToken(){
+            //given
+            Session session = new Session(new Date(),new Date());
+            User user = new User("id","password","nickname");
+            String csrfToekn = UUID.randomUUID().toString();
+            String otherCsrfToken = UUID.randomUUID().toString();
+            session.set("user",user);
+            session.set("csrfToken",otherCsrfToken);
+
+            BoardRegist board = new BoardRegist("title","content",csrfToekn);
+
+            //when
+            String path = boardController.registBoard(session, board);
+
+            //then
+            assertEquals("redirect:/",path);
+            long size = boardDatabase.findAll()
+                    .stream().filter(b -> b.getWriter().equals("id"))
+                    .count();
+            assertEquals(0,size);
+        }
+
+        @Test
+        void nullCsrfToken(){
+            //given
+            Session session = new Session(new Date(),new Date());
+            User user = new User("id","password","nickname");
+            String csrfToekn = UUID.randomUUID().toString();
+            session.set("user",user);
+
+            BoardRegist board = new BoardRegist("title","content",csrfToekn);
+
+            //when
+            String path = boardController.registBoard(session, board);
+
+            //then
+            assertEquals("redirect:/",path);
+            long size = boardDatabase.findAll()
+                    .stream().filter(b -> b.getWriter().equals("id"))
+                    .count();
+            assertEquals(0,size);
+        }
+
+        @Test
+        void nullSession(){
+            //given
+            User user = new User("id","password","nickname");
+            String csrfToken = UUID.randomUUID().toString();
+
+            BoardRegist board = new BoardRegist("title","content",csrfToken);
+
+            //when
+            String path = boardController.registBoard(null, board);
+
+            //then
+            assertEquals("redirect:/login",path);
+            long size = boardDatabase.findAll()
+                    .stream().filter(b -> b.getWriter().equals("id"))
+                    .count();
+            assertEquals(0,size);
+        }
+
+        @Test
+        void nullUserInSession(){
+            //given
+            Session session = new Session(new Date(),new Date());
+            String csrfToken = UUID.randomUUID().toString();
+            session.set("csrfToken",csrfToken);
+
+            BoardRegist board = new BoardRegist("title","content",csrfToken);
+
+            //when
+            String path = boardController.registBoard(session, board);
+
+            //then
+            assertEquals("redirect:/login",path);
+            long size = boardDatabase.findAll()
+                    .stream().filter(b -> b.getWriter().equals("id"))
+                    .count();
+            assertEquals(0,size);
+        }
+
+        @Test
+        void nullCsrfTokenInBoard(){
+            //given
+            Session session = new Session(new Date(),new Date());
+            User user = new User("id","password","nickname");
+            String csrfToken = UUID.randomUUID().toString();
+            session.set("user",user);
+            session.set("csrfToken",csrfToken);
+
+            BoardRegist board = new BoardRegist("title","content",null);
+
+            //when
+            String path = boardController.registBoard(session, board);
+
+            //then
+            assertEquals("redirect:/",path);
+            long size = boardDatabase.findAll()
+                    .stream().filter(b -> b.getWriter().equals("id"))
+                    .count();
+            assertEquals(0,size);
         }
     }
 }
