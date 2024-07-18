647667e5d4aff131c7e94838cad1c600b1aaa3ac
Hyeon-Uk <rlagusdnr120@gmail.com>
2024-07-19 00:55:07 +0900

    [Refactor] 코드를 깔끔하게 정리하는 리팩토링

--- src/main/java/codesquad/was/http/handler/SocketHandler.java
+++ src/main/java/codesquad/was/http/handler/SocketHandler.java
@@ -54,9 +54,6 @@ public class SocketHandler implements Runnable {
                 response.addCookie(new Cookie("SID", session.getId()));
             }
 
-//            RequestHandler handler = requestHandlerMapper.getRequestHandler(request.getUri(),request.getMethod());
-//            logger.info("handler : {}",handler);
-//            handler.handle(request, response);
             requestHandler.handle(request,response);
 
             sendResponse(response, socket);
