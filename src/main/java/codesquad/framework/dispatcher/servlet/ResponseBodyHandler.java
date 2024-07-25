c4e4fd96b62e8d23ec9c5f3fc3ae6440400e2568
Hyeon-Uk <rlagusdnr120@gmail.com>
2024-07-25 09:58:54 +0900

    [Feat] ResponseBodyHandler 구현
    @ResponseBody가 붙은 메서드를 핸들링하기 위한 handler 구현.
    return 값을 objectMapper로 파싱하여 response 하도록 설정

---
+++ src/main/java/codesquad/framework/dispatcher/servlet/ResponseBodyHandler.java
@@ -0,0 +1,66 @@
+package codesquad.framework.dispatcher.servlet;
+
+import codesquad.framework.ObjectMapper;
+import codesquad.framework.dispatcher.mv.Model;
+import codesquad.framework.resolver.ArgumentResolver;
+import codesquad.was.http.exception.HttpException;
+import codesquad.was.http.exception.HttpInternalServerErrorException;
+import codesquad.was.http.handler.RequestHandler;
+import codesquad.was.http.message.request.HttpRequest;
+import codesquad.was.http.message.response.HttpResponse;
+import codesquad.was.http.message.response.HttpStatus;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class ResponseBodyHandler implements RequestHandler {
+    private final Logger logger = LoggerFactory.getLogger(ResponseBodyHandler.class);
+    private final Object controller;
+    private final Method method;
+    private final HttpStatus status;
+    private final ArgumentResolver argumentResolver;
+    private final ObjectMapper objectMapper;
+
+    public ResponseBodyHandler(Object controller,
+                      Method method,
+                      HttpStatus status,
+                      ArgumentResolver argumentResolver,
+                               ObjectMapper objectMapper) {
+        this.controller = controller;
+        this.method = method;
+        this.status = status;
+        this.argumentResolver = argumentResolver;
+        this.objectMapper = objectMapper;
+    }
+
+    @Override
+    public void handle(HttpRequest req, HttpResponse res) {
+        try{
+            Model model = new Model();
+            Object[] arguments = argumentResolver.resolveArguments(method,req,res,model);
+            Class<?> returnType = method.getReturnType();
+            Object result = method.invoke(controller, arguments);
+            if(Void.class.equals(returnType)){
+                res.setStatus(HttpStatus.NO_CONTENT);
+            }
+            else {
+                res.setStatus(status);
+                res.setHeader("Content-Type","application/json;charset=UTF-8");
+                res.setBody(objectMapper.toJson(result).getBytes());
+            }
+        } catch (InvocationTargetException e) {
+            Throwable originalException = e.getTargetException();
+            if(originalException instanceof HttpException){
+                throw (HttpException) originalException;
+            }
+            else{
+                throw new HttpInternalServerErrorException(originalException.getMessage());
+            }
+        } catch (Exception e) {
+            throw new HttpInternalServerErrorException(e.getMessage());
+        }
+    }
+
+}
